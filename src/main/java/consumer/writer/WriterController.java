c4647f1523117b625daa09244ae0f8f35bd211ca
Fake <72094885+i-am-fake@users.noreply.github.com>
2021-12-31 09:30:46 +0100

    RocksDb database will be created in working dir

--- src/main/java/consumer/writer/WriterController.java
+++ src/main/java/consumer/writer/WriterController.java
@@ -11,12 +11,13 @@ import java.util.concurrent.BlockingQueue;
 public class WriterController {
 
     static final int NUMBER_OF_WRITERS = 3;
+
     private final List<Worker<?>> writers = new ArrayList<>();
     private final List<Thread> threads = new ArrayList<>();
 
     public void start(BlockingQueue<ProductX> queue) {
         for (int i = 0; i < NUMBER_OF_WRITERS; i++) {
-            Worker<ProductX> writer = new Worker<>(queue, new Dao<>(new RocksConn(System.getProperty("user.home") + "/Desktop/database")));
+            Worker<ProductX> writer = new Worker<>(queue, new Dao<>(new RocksConn("database")));
             writers.add(writer);
             threads.add(new Thread(writer, "Writer num: " + i + "."));
         }
